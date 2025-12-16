8dd71dd03c15fdbb462aed9e9bd38cf6dace9d41
Marcos Aleixo <maknytz@gmail.com>
2025-12-16 20:47:05 -0300

    Criando atributos e objetos para a classe

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -2,6 +2,22 @@
 
 namespace Marco\DioPhpPoo;
 
-class ContaBancaria {
+class ContaBancaria 
+{
+    public string $banco = "Banco Inter";
+    public string $nomeTitular = "Gustavo Fraga";
+    public string $numeroAgencia = "8244";
+    public string $numeroConta = "57354-10";
+    public float $saldo = 0;
 
+    public function exibirDadosDaConta() :array
+    {
+        return [
+            'banco' => $this->banco,
+            'nomeTitular' => $this->nomeTitular,
+            'numeroAgencia' => $this->numeroAgencia,
+            'numeroConta' => $this->numeroConta,
+            'saldo' => $this->saldo
+        ];
+    }
 }
