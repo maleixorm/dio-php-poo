0a6e830b672d9fdfcda9794cb393ad2a3d82938c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-17 03:09:57 -0300

    Criando getter e setters para atribuir e recuperar valores em atributos privados

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -6,17 +6,27 @@ namespace Marco\DioPhpPoo;
 
 class ContaBancaria 
 {
-    public string $banco;
+    private string $banco;
     private string $nomeTitular = "Gustavo Fraga";
     private string $numeroAgencia = "8244-5";
     private string $numeroConta = "57354-10";
     private float $saldo = 0;
 
-    public function exibirDadosDaConta() :array
+    public function exibirDadosDaConta():array
     {
         return [
             'nomeTitular' => $this->nomeTitular,
             'numeroConta' => $this->numeroConta
         ];
+    }
+
+    public function setBanco(string $banco):void
+    {
+        $this->banco = $banco;
+    }
+
+    public function getBanco():string
+    {
+        return $this->banco;
     }
 }
