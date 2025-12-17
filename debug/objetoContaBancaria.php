5f354080fda4195ae60b11a1e0da06214a7c0349
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-17 03:10:35 -0300

    Utilizando os getters e setters em Conta Bancária

--- debug/objetoContaBancaria.php
+++ debug/objetoContaBancaria.php
@@ -5,5 +5,5 @@ require __DIR__ . '/../vendor/autoload.php';
 use Marco\DioPhpPoo\ContaBancaria;
 
 $conta = new ContaBancaria();
-$conta->banco =  "Banco do Brasil";
-var_dump($conta->banco);
+$conta->setBanco("Banco do Brasil");
+var_dump($conta->getBanco());
