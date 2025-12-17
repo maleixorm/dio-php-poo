6e0871dbb3be922307424b9e395a9548ceae4e91
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-17 03:24:05 -0300

    Implementando os atributos da conta com o construtor

--- debug/objetoContaBancaria.php
+++ debug/objetoContaBancaria.php
@@ -4,6 +4,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Marco\DioPhpPoo\ContaBancaria;
 
-$conta = new ContaBancaria();
-$conta->setBanco("Banco do Brasil");
-var_dump($conta->getBanco());
+$conta = new ContaBancaria('Banco do Brasil', 'Gustavo Fraga', '8244-5', '57354-7', 0);
+var_dump($conta->exibirDadosDaConta());
