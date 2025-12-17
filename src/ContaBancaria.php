954889a470c482f3fa913c569aabd60c4fa1c920
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-17 03:17:22 -0300

    Criando o método construtor da classe

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -7,10 +7,19 @@ namespace Marco\DioPhpPoo;
 class ContaBancaria 
 {
     private string $banco;
-    private string $nomeTitular = "Gustavo Fraga";
-    private string $numeroAgencia = "8244-5";
-    private string $numeroConta = "57354-10";
-    private float $saldo = 0;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
+
+    public function __construct(string $banco, string $nomeTitular, string $numeroAgencia, string $numeroConta, float $saldo)
+    {
+        $this->banco = $banco;
+        $this->nomeTitular = $nomeTitular;
+        $this->numeroAgencia = $numeroAgencia;
+        $this->numeroConta = $numeroConta;
+        $this->saldo = $saldo;
+    }
 
     public function exibirDadosDaConta():array
     {
